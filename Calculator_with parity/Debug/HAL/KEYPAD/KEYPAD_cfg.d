58451a4196800cf900c6080eb5ce40ff2722d981
Khloud khaled Atia <72269263+Khloud1999@users.noreply.github.com>
2023-01-14 03:02:12 +0200

    Delete Calculator_with parity directory

--- Calculator_with parity/Debug/HAL/KEYPAD/KEYPAD_cfg.d
+++
@@ -1,7 +0,0 @@
-HAL/KEYPAD/KEYPAD_cfg.o HAL/KEYPAD/KEYPAD_cfg.o:  \
- ../HAL/KEYPAD/KEYPAD_cfg.c ../HAL/KEYPAD/../../SERV/STD_TYPES.h \
-  ../HAL/KEYPAD/../../MCAL/DIO/dio_int.h
-
-../HAL/KEYPAD/../../SERV/STD_TYPES.h:
-
-../HAL/KEYPAD/../../MCAL/DIO/dio_int.h:
